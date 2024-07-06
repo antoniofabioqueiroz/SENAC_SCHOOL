c2cb2b9729af3cd522934cfb1cb6d5c4d0f8fcd1
Rynilan <ilanvitorsantos@gmail.com>
2024-07-06 02:02:34 -0300

    Update ListarNotas.py
    Exibição de diferente valor caso o aluno tenha sido cadastrado, mas não sua nota

--- ListarNotas.py
+++ ListarNotas.py
@@ -8,8 +8,14 @@ def RelNota(aluno, vet, op):
             print()
             print("Aluno: ", aluno[i][0])
             for l in range(0, 4):
-                print(l+1, "° Nota: {:.2f}".format(vet[i][l]))
-            print("Média:   {:.2f}".format(vet[i][4]))
+                if vet[i][l] is not None:
+                    print(l+1, "° Nota: {:.2f}".format(vet[i][l]))
+                else:
+                    print(l+1, "º Nota: {}".format("Nota não cadastrada"))
+            if vet[i][4] is not None:
+                print("Média:   {:.2f}".format(vet[i][4]))
+            else:
+                print("Média:   {}".format("Notas não cadastradas."))
     print("Para sair pressione 'Enter'.")
     i = input()
  
